8c40421d56d1890a6bd3d4a1b13ddd4ac06dd957
Stas Ukolov <ukoloff@gmail.com>
2018-06-06 14:14:21 +0500

    Try to compile

--- bin/build.bat
+++ bin/build.bat
@@ -1,4 +1,8 @@
 For /f %%a in ('node -p process.arch') do set NArch=%%a
-Echo Node architecture: %Narch%
+Echo Node architecture: %NArch%
 node -v
 npm -v
+git clone src %NArch%
+cd %NArch%
+npm install
+dir build\Release
